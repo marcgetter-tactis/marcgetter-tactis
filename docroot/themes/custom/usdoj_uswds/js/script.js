efc941f348ccb311d92a7f284e94949dda244eed
Josh C <josh.ciolkosz@tactis.com>
2022-01-24 11:24:27 -0600

    Slick slider and styles working

--- docroot/themes/custom/usdoj_uswds/js/script.js
+++ docroot/themes/custom/usdoj_uswds/js/script.js
@@ -2,13 +2,23 @@
   Drupal.behaviors.executeSlick = {
     attach: function (context, settings) {
 
-      $('.hero-slideshow').once().slick({
-        infinite: true,
-        dots: true,
-        autoplay: true,
-        arrows: false,
-        autoplaySpeed: 6000,
-      });
+      once('executeSlick', 'html', context).forEach( function (element) {
+
+        $('.hero-slideshow').slick({
+          infinite: true,
+          dots: true,
+          autoplay: true,
+          arrows: false,
+          autoplaySpeed: 6000,
+
+        });
+
+        //* Pause the slideshow when the dot is clicked.
+        $('.hero-slideshow .slick-dots button').on("click", function() {
+          $(this).closest('.hero-slideshow').slick('slickPause');
+        });
+      })
+
     }
   };
 })(jQuery);

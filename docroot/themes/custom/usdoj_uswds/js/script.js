fe76c24826ce027bf0e4d2e4a4695fa0345a2459
gb-tactis <grant.barnette@tactis.com>
2022-02-03 16:57:00 -0500

    JGDR-149 tabs for desktop

--- docroot/themes/custom/usdoj_uswds/js/script.js
+++ docroot/themes/custom/usdoj_uswds/js/script.js
@@ -38,4 +38,17 @@
 
     }
   };
+
+  $('.panel .block-tab').click(function() {
+    var i = $(this).attr('id');
+
+    if ($('.panel .block-tab').hasClass('active')) {
+      $(this).removeClass('active');
+      $('.panel-content').hide();
+    }
+    
+    $(this).addClass('active');
+    $('#' + i + '-body').fadeIn('slow');
+  })
+
 })(jQuery);

5986000b77f4f0c7e00a9a0ac67f9cfe94603a39
John Cunningham <john.cunningham@acquia.com>
2022-03-02 12:07:40 -0500

    JGDR-274: Update SSO configuration.

--- simplesamlphp/config/authsources.php
+++ simplesamlphp/config/authsources.php
@@ -6,6 +6,8 @@ $config = [
     ],
     'default-sp' => [
         'saml:SP',
+        'privatekey' => '../certs/saml.pem',
+        'certificate' => '../certs/saml.crt',
         'entityid' => 'http://dojsts1-t.idms.justice.gov/adfs/services/trust',
         'idp' => 'http://dojsts1-t.idms.justice.gov/adfs/services/trust',
     ],
